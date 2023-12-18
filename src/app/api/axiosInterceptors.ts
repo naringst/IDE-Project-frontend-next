9e979771fa70a354afa4f6197afe0309d4e5aa71
nari <naringst@gmail.com>
2023-12-18 18:53:54 +0900

    feat : server 설정

--- src/app/api/axiosInterceptors.ts
+++ src/app/api/axiosInterceptors.ts
@@ -1,12 +1,13 @@
-import useTokenStore from '@/store/useTokenStore';
+import useTokenStore from '../../store/useTokenStore';
 import { refreshToken } from './auth';
 import axiosInstance from './axiosInstance';
 
 axiosInstance.interceptors.request.use(async config => {
   await refreshToken();
   const token = useTokenStore.getState().accessToken;
+  console.log(token);
   if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+    config.headers['Authorization'] = token;
   }
   return config;
 });
